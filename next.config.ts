9c2eba7263b5162e9eb30a34355a65d69373ac4d
Andrew Hipp <andrewehippgmail.com>
2025-08-04 10:06:24 -0700

    feat: static export working

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "images.ctfassets.net", // Contentful's image CDN hostname
-                port: "", // Leave empty unless a specific port is used
-                pathname: "/**", // Allows any path under the hostname
-            },
-            {
-                protocol: "https",
-                hostname: "downloads.ctfassets.net", // Contentful's image CDN hostname
-                port: "", // Leave empty unless a specific port is used
-                pathname: "/**", // Allows any path under the hostname
-            },
-        ],
-    },
+    output: 'export',
+    trailingSlash: true,
 };
 
 export default nextConfig;
